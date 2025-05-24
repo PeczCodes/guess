6ef92ebe304afe9cf0111b19f5f7f367a14c601b
PeczCodes <peculiaramile@gmail.com>
2025-05-24 15:44:35 +0100

    deployment again

--- app/page.tsx
+++ app/page.tsx
@@ -15,15 +15,6 @@ const api = "/api";
 const wordLength = 5;
 const maxGuesses = 6;
 
-function usePrevious<T>(value: T): T | undefined {
-	const ref = useRef<T>(undefined);
-	useEffect(() => {
-		ref.current = value;
-	}, [value]);
-	return ref.current;
-}
-
-
 type pages = "game" | "help"
 
 const Page = () => {
